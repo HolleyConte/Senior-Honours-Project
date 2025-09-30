f0af10b0bfaa7528a80e04a611b5317f9edbe416
Holley <holleyconte@gmail.com>
2025-09-30 13:59:01 +0100

    wrote this for the Heat module in "likelihood_temperature.py" to increase the temperature. Just took the log likelihood, divided by Temperature, and then returned it into the pipeline

--- heat/likelihood_temperature.py
+++ heat/likelihood_temperature.py
@@ -13,15 +13,20 @@ def setup(options):
 def execute(block, config):
     section, name, temperature = config
 
-    # 1. Get the current log-likelihood value from the block using the section
-    # and name as keys
+    # 1. Get the current log-likelihood value from the block using the section and name as keys
+    log_likelihood = block[section, name]
+
 
     # 2. Modify the log-likelihood as we want
+    heated_log_likelihood = log_likelihood / temperature
+
 
     # 3. Save it back to the block
+    block[section, name] = heated_log_likelihood
+
 
     return 0
 
 
-
-#testing
+def cleanup(config):
+    pass
